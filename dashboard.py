e2b2a09da2072c75cb14fd5c91f0807f4bceede2
Ibnu Khaidar <Ibnukhaidar@gmail.com>
2024-03-03 17:04:11 +0700

    added a Screenshot of air quality index and adding read dataframe from output file

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,12 @@
+# import yang diperlukan package
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
+
+# import dataset
+aq_df = pd.read_csv("output.csv", delimiter=',')
+
+# group temprature by hour
 
 
 
@@ -9,6 +15,9 @@ import matplotlib.pyplot as plt
 
 def main():
     st.title('Kualitas Udara')
+
+    st.text('Standart Air Quality index')
+    st.image('Screenshot 2024-03-03 141028.png')
 
 
 main()
